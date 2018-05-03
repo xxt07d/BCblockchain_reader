b9562f2696a1ce6280720f15edee571ef4dbaca8
oZs <oliver.zsamar@gmail.com>
2018-05-04 00:30:25 +0200

    Update BlockTimeDifferenceAverage.java

--- src/main/java/bitcoin/BlockTimeDifferenceAverage.java
+++ src/main/java/bitcoin/BlockTimeDifferenceAverage.java
@@ -2,6 +2,11 @@ package bitcoin;
 
 import java.text.SimpleDateFormat;
 
+/**
+ * Az osztály arra való, hogy blokkok között eltelt átlagos időt számoljunk egy adott napra.
+ * Eltárolja a legutoljára beadott blokk idejét, ami elegendő, mivel a blokkok sorfojtonosan vannak.
+ * Azt, hogy melyik nappal dolgozik, valamint, hogy mennyi blokk átlagát számolja.
+ */
 public class BlockTimeDifferenceAverage {
     private long lastBlockTime;
     private String day = null;
@@ -9,9 +14,18 @@ public class BlockTimeDifferenceAverage {
     private long differenceSum = 0;
 
 
+    /**
+     * Konstruktor, jelenleg nem csinál semmi különöset.
+     */
     public BlockTimeDifferenceAverage(){
     }
 
+    /**
+     * Ez a függvény szolgál arra, hogy hozzáadjunk további blokkokat.
+     * Csak akkor adunk hozzá, ha eddig nem adtunk hozzá egyet sem, vagy ha megegyezik a blokk keletkezési napja
+     * az eltárolt (day) nappal.
+     * @param blockTime
+     */
     public void addBlockTime(long blockTime){
         if(day == null){
             this.lastBlockTime = blockTime;
@@ -24,6 +38,10 @@ public class BlockTimeDifferenceAverage {
         }
     }
 
+    /**
+     * Egy wrapper objektumban kiszámolja az eddigi átlagot
+     * @return Az átlag.
+     */
     public Double makeAverage(){
         return differenceSum/(double)blockCounter;
     }
